6096be57c828b55bafa71f67468ef0562148af80
Andrew Scott <ascott02@gmail.com>
2025-08-19 18:32:21 -0400

    sync sh script

--- run_deepspeed_training.sh
+++ run_deepspeed_training.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-export CUDA_VISIBLE_DEVICES="3,4,0"
-export MASTER_ADDR=130.212.4.133
-export MASTER_PORT=29500
+# export CUDA_VISIBLE_DEVICES="3,4,0"
+# export MASTER_ADDR=130.212.4.133
+# export MASTER_PORT=29500
 # export NCCL_DEBUG=INFO
 # export CUDA_HOME="/usr/"
 # export NCCL_DEBUG_SUBSYS=ALL
-export NCCL_SOCKET_IFNAME=eno1
+# export NCCL_SOCKET_IFNAME=eno1
 
 ZERO_STAGE=1
 NUM_EPOCHS=2
 NUM_CAPTIONS=10
-SUBSET_SIZE=0.1
+SUBSET_SIZE=0.10
 
 NUM_HIDDEN_LAYERS=12
 HIDDEN_SIZE_ENCODER=768
 GRADIENT_ACCUMULATION_STEPS=4
 
-EXPERIMENT_NAME="VATEX-single-loaders"
-DATA_DIR="/data2/juve/dataset/vatex/npz_datasets/VATEX_8_frames"
-OUTPUT_DIR="/data2/juve/training_artifacts/"
+EXPERIMENT_NAME="VATEX-psc"
+DATA_DIR="/ocean/projects/cis240146p/shared/data/VATEX_8_frames"
+OUTPUT_DIR="/ocean/projects/cis240146p/scotta/training_artifacts/"
 
 NUM_NODES=1   # Number of nodes
 NUM_GPU=3     # Number of GPUs per node
@@ -28,7 +28,7 @@ MICRO_BATCH=1 # PER GPU
 WORLD_SIZE=$((NUM_NODES * NUM_GPU))
 BATCH_SIZE=$((WORLD_SIZE * MICRO_BATCH)) # Total batch size acrross all GPUs and nodes
 
-PRETRAINED_MODEL="/data1/juve/training_artifacts/vatex_100/polynomial/vatex_1.0prcnt_s24_10caps_lr1e-05_30_epochs_power_1.4_end_1e_8/model_saved_files/epoch_3"
+# PRETRAINED_MODEL="/data1/juve/training_artifacts/vatex_100/polynomial/vatex_1.0prcnt_s24_10caps_lr1e-05_30_epochs_power_1.4_end_1e_8/model_saved_files/epoch_3"
 PRETRAINED_ENC="facebook/timesformer-base-finetuned-k600"
 PRETRAINED_DEC="openai-community/gpt2"
 IMAGE_PP="MCG-NJU/videomae-base"
@@ -36,7 +36,7 @@ TOKENIZER="gpt2"
 
 # torchrun --nproc_per_node=3 --nnodes=1 --node_rank=0 main_deepspeed.py  --num_gpus $NUM_GPU \
     # --pretrained_model $PRETRAINED_MODEL \
-deepspeed --master_addr $MASTER_ADDR --master_port $MASTER_PORT main_deepspeed_gpt5_refactor.py --num_gpus $NUM_GPU \
+deepspeed main_deepspeed_gpt5_refactor.py --num_gpus $NUM_GPU \
     --world_size $WORLD_SIZE -ep $NUM_EPOCHS -ss $SUBSET_SIZE --num_captions $NUM_CAPTIONS \
     --experiment_name $EXPERIMENT_NAME --batch_size $BATCH_SIZE \
     --data_dir $DATA_DIR --output_dir $OUTPUT_DIR \
