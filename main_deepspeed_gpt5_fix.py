01f1185f1c7c98cb35a7bb5c0dbd9bb8bc70c5ae
juveb7 <juveb6@gmail.com>
2025-09-01 14:54:22 -0700

    set up the metrics & qualitative report

--- main_deepspeed_gpt5_fix.py
+++ main_deepspeed_gpt5_fix.py
@@ -5,9 +5,11 @@ import pathlib
 import numpy as np
 import torch
 import torch.nn as nn
+import io
 import argparse
 import random
 import wandb
+import base64
 from torch.utils.data import Dataset, DataLoader
 from torch.utils.data.dataloader import default_collate
 from torch.utils.data.distributed import DistributedSampler
@@ -20,6 +22,7 @@ from transformers import (
     GPT2Config,
     TimesformerModel
 )
+from torchvision import transforms
 import torch.nn.functional as F
 import torch.distributed as dist
 import deepspeed
@@ -29,7 +32,11 @@ from deepspeed.pipe import PipelineModule, TiedLayerSpec
 from deepspeed.utils import RepeatingLoader
 from transformers.modeling_outputs import BaseModelOutput
 from transformers import get_scheduler
+from pycocoevalcap.meteor.meteor import Meteor
+from pycocoevalcap.rouge.rouge import Rouge
 from pycocoevalcap.cider.cider import Cider
+from pycocoevalcap.spice.spice import Spice
+from torcheval.metrics.text import BLEUScore
 import math
 
 # Argument parser
@@ -166,6 +173,8 @@ def main():
     # Dynamic globals - use as few as possible
     # att_type = {'divided_space_time': 'dst', 'space_only': 'so', 'joint_space_time': 'jst'}
     experiment_name = f"{args.experiment_name_prefix}_ws{dist.get_world_size()}_nc{args.num_captions}_ep{args.num_epochs}_ss{args.subsample_size}_nl{args.num_hidden_layers}_hs{args.hidden_size_encoder}_nf{args.num_frames_encoder}_ps{args.patch_size_encoder}_lr{args.learning_rate}_bs{args.batch_size}_rs{args.random_seed}"
+    # experiment_name = f"{args.experiment_name_prefix}_ws{dist.get_world_size()}_nc{args.num_captions}_ep{args.num_epochs}_ss0.25_nl{args.num_hidden_layers}_hs{args.hidden_size_encoder}_nf{args.num_frames_encoder}_ps{args.patch_size_encoder}_lr{args.learning_rate}_bs{args.batch_size}_rs{args.random_seed}"
+
     # experiment_name = "placeholder_v3"
     experiment_output_dir = os.path.join(args.output_dir, experiment_name)
 
@@ -181,8 +190,6 @@ def main():
         "fp16": { "enabled": args.fp16_enabled, "auto_cast": args.fp16_autocast, },
         "pipeline_parallel_size": dist.get_world_size(),
     }
-
-    
 
     # Load pretrained components
     image_processor = AutoImageProcessor.from_pretrained(args.image_preprocessor)
@@ -269,7 +276,7 @@ def main():
 
     train_dataset = NPZDataset(os.path.join(args.data_dir, 'train'), args.num_captions, args.subsample_size, tokenizer)
     val_dataset = NPZDataset(os.path.join(args.data_dir, 'val'), args.num_captions, args.subsample_size, tokenizer)
-    test_dataset = NPZDataset(os.path.join(args.data_dir, 'test'), 1, 1.0, tokenizer) # TODO: Change this back to args.subsample_size
+    test_dataset = NPZDataset(os.path.join(args.data_dir, 'test'), 1, 0.001, tokenizer) # TODO: Change this back to args.subsample_size
     # val_sampler = DistributedSampler(val_dataset, num_replicas=dist.get_world_size(), rank=dist.get_rank(), shuffle=False)
     
     # -------------------------------------------------------------
@@ -676,17 +683,8 @@ def main():
 
     # labels = [24, 554, 50256, 50256, 50256] => [24, 554, 50256, -100, -100]
     def compute_loss(logits, labels):
-        # output is (logits, keep_labels) from FinalWrapper
-        # labels = labels.to(logits.device).long()
-        # mask AFTER first EOS
-        # masked_labels = labels.masked_fill(~keep_labels.bool(), -100)
-        # loss_fct = torch.nn.CrossEntropyLoss(ignore_index=-100)
-        # return loss_fct(logits.view(-1, logits.size(-1)),
-        #                 masked_labels.view(-1))
-
         loss_fct = torch.nn.CrossEntropyLoss()
-        return loss_fct(logits.view(-1, logits.size(-1)),
-                        labels.view(-1))
+        return loss_fct(logits.view(-1, logits.size(-1)), labels.view(-1))
     
     # if args.fp16_enabled:
     #     hf_model = hf_model.half()
@@ -871,15 +869,16 @@ def main():
     dist.barrier()
    
     if args.do_test:
-        total_test_loss = 0
         predicted_captions = []
-        predicted_tokens = []
         ground_truth_captions = []
-        ground_truth_tokens = []
         all_filenames = []
+        uids = []
         
         # metric libs
-        cider_metric = Cider()
+        bleu1_metric = BLEUScore(n_gram=1)
+        bleu2_metric = BLEUScore(n_gram=2)
+        bleu3_metric = BLEUScore(n_gram=3)
+        bleu4_metric = BLEUScore(n_gram=4)
 
         deep_speed_model_engine.eval()
         test_iter = iter(RepeatingLoader(test_dataloader))
@@ -904,44 +903,94 @@ def main():
             #                                top_k=args.top_k, top_p=args.top_p, temperature=args.temperature)
 
             gts = [ test_dataset.get_gt_caption(m[2].item()) for m in metadata ]
-            # for g_pred, top_kp_pred, g in zip(greedy_preds, top_kp_preds, gts):
-            #     print(f"greedy: {g_pred}\ntop_kp: {top_kp_pred}\ngts: {g}")
-            
             predicted_captions.extend(greedy_preds)
-            ground_truth_tokens.extend(gts)
-            # filename = decode_metadata_tensor
-            # all_filenames.extend(batch['filenames'])
+            ground_truth_captions.extend(gts)
+            for m in metadata:
+                info = test_dataset.decode_metadata_tensor(m)
+                # unique, stable key per sample (filename + caption_idx)
+                uids.append(f"{info['filename']}::{info['caption_idx']}")
+                all_filenames.append(info['filename'])
 
         if deep_speed_model_engine.is_last_stage():
             metrics_dict = {}       
-            # metrics_dict["avg_test_loss"] = total_test_loss / len(test_dataloader)
 
-            ground_truth_captions_flattened = [[x.replace('\n', ' ').strip()] for x in ground_truth_captions]
-            predicted_captions_flattened = [[x.replace('\n', ' ').strip()] for x in predicted_captions]
-            ground_truth_captions_dict = dict(zip(all_filenames, ground_truth_captions_flattened))
-            predicted_captions_dict = dict((zip(all_filenames, predicted_captions_flattened)))
-            
-            # metrics_dict["blue1_score"] = bleu1_metric.update(predicted_captions, ground_truth_captions).compute().item()
-            # metrics_dict["blue2_score"] = bleu2_metric.update(predicted_captions, ground_truth_captions).compute().item()
-            # metrics_dict["blue3_score"] = bleu3_metric.update(predicted_captions, ground_truth_captions).compute().item()
-            # metrics_dict["blue4_score"] = bleu4_metric.update(predicted_captions, ground_truth_captions).compute().item()
-            # metrics_dict["perplexity_score"] = perplexity_metric.compute().item()
-            # metrics_dict["word_error_rate_score"] = word_error_rate_metric.update(predicted_captions, ground_truth_captions).compute().item()
-            # metrics_dict["word_info_lost_score"] = word_info_lost_metric.update(predicted_captions, ground_truth_captions).compute().item()
-            # metrics_dict["word_info_preserved_score"] = word_info_preserved_metric.update(predicted_captions, ground_truth_captions).compute().item()
+            ground_truth_captions_flattened = [str(x).replace('\n', ' ').strip() for x in ground_truth_captions]
+            predicted_captions_flattened = [str(x).replace('\n', ' ').strip() for x in predicted_captions]
+            # ground_truth_captions_dict = dict(zip(all_filenames, ground_truth_captions_flattened))
+            # predicted_captions_dict = dict((zip(all_filenames, predicted_captions_flattened)))
+            ground_truth_captions_dict = {k: [v] for k, v in zip(uids, ground_truth_captions_flattened)}
+            predicted_captions_dict    = {k: [v] for k, v in zip(uids, predicted_captions_flattened)}
 
-            # print(f"\n\nDEBUG ground_truth_captions_dict: {ground_truth_captions_dict}\n\n")
-            # print(f"\n\nDEBUG predicted_captions_dict: {predicted_captions_dict}\n\n")
-            metrics_dict["cider_score"], _ = Cider().compute_score(ground_truth_captions_dict, predicted_captions_dict)
-            # metrics_dict["meteor_score"], _ = Meteor().compute_score(ground_truth_captions_dict, predicted_captions_dict)
-            # metrics_dict["rouge_score"], _ = Rouge().compute_score(ground_truth_captions_dict, predicted_captions_dict)
-            # metrics_dict["spice_score"], spice_scores = Spice().compute_score(ground_truth_captions_dict, predicted_captions_dict)
+            metrics_dict["blue1_score"] = bleu1_metric.update(predicted_captions, ground_truth_captions).compute().item()
+            metrics_dict["blue2_score"] = bleu2_metric.update(predicted_captions, ground_truth_captions).compute().item()
+            metrics_dict["blue3_score"] = bleu3_metric.update(predicted_captions, ground_truth_captions).compute().item()
+            metrics_dict["blue4_score"] = bleu4_metric.update(predicted_captions, ground_truth_captions).compute().item()
+            metrics_dict["cider_score"], cider_scores = Cider().compute_score(ground_truth_captions_dict, predicted_captions_dict)
+            metrics_dict["meteor_score"], meteor_scores = Meteor().compute_score(ground_truth_captions_dict, predicted_captions_dict)
+            metrics_dict["rouge_score"], rouge_scores = Rouge().compute_score(ground_truth_captions_dict, predicted_captions_dict)
+            metrics_dict["spice_score"], spice_scores = Spice().compute_score(ground_truth_captions_dict, predicted_captions_dict)
             wandb.log(metrics_dict)
         
     dist.barrier()
 
     if deep_speed_model_engine.is_last_stage():
         wandb.finish()
+
+        # Run the qualitative if we are doing that
+        os.makedirs(experiment_output_dir, exist_ok=True)
+        with open(os.path.join(experiment_output_dir, experiment_name +".csv"), 'w') as f:
+            f.write("uid,filename,pred,gt\n")
+            for i in range(len(uids)):
+                f.write(f"{uids[i]},{all_filenames[i]},{predicted_captions[i]},{ground_truth_captions[i]}\n")
+
+        mean = torch.tensor(image_processor.image_mean).view(1, 3, 1, 1)
+        std = torch.tensor(image_processor.image_std).view(1, 3, 1, 1)
+
+        with open(os.path.join(experiment_output_dir, experiment_name + ".html"), 'w') as f:
+            f.write(f"""<!DOCTYPE html>
+                        <html><head><style>
+                        .sample {{  width: 1024px; border: 1px solid black; padding: 10px; margin: 10px; }}
+
+                        .grid-container {{
+                            display: grid;
+                            grid-template-columns: repeat(4, 1fr); /* Creates 4 equal columns */
+                            grid-template-rows: repeat(2, 1fr); /* Creates 2 equal rows */
+                            place-items: center;                                    
+                            gap: 10px; /* Optional: Add spacing between grid items */
+                        }}
+
+                        .grid-container img {{
+                            width: 224px; /* Make images fill the grid cells */                                 
+                            height: 224px;                                                                      
+                            object-fit: cover; /* Maintain aspect ratio and cover the cell */
+                        }}
+                        </style></head>
+                        <body>
+                    """)
+            for i in range(min(len(uids), args.num_qualitative)):
+                uid = uids[i]
+                filename = all_filenames[i]
+                npz_data = np.load(os.path.join(args.data_dir, "test", filename))
+                processed_images = torch.tensor(npz_data['arr_0'])
+                unprocessed_images = processed_images * std + mean
+
+                pred = predicted_captions_dict[uid][0]
+                gt   = ground_truth_captions_dict[uid][0]
+
+                f.write(f"<div class='sample'><p>{i}, {uid}<br>Predicted Caption: {pred}<br>Ground-Truth Caption: {gt}</p>\n<div class='grid-container'>\n")
+                # for j in range(npz_data['arr_0'].shape[0]):
+                for j in range(unprocessed_images.shape[0]):
+                    an_image = unprocessed_images[j]
+                    transform = transforms.ToPILImage()
+                    pil_image = transform(an_image)
+                    buffer = io.BytesIO()
+                    pil_image.save(buffer, format="PNG")
+                    buffer.seek(0) # Rewind the buffer to the beginning
+                    base64_string = base64.b64encode(buffer.read()).decode()
+                    img_tag = f'<img src="data:image/png;base64,{base64_string}">' 
+                    f.write(f"{img_tag}\n")
+                f.write("</div></div>\n")
+            f.write(f"</body></html>")
 
 if __name__ == '__main__':
     main()
