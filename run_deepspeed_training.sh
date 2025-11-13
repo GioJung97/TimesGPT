57dbcfc4f9485cdc5e6c1cfa7892d71920bbb525
juveb7 <juveb6@gmail.com>
2025-11-13 12:10:57 -0800

    fixed wandb and now doing test set every epoch..

--- run_deepspeed_training.sh
+++ run_deepspeed_training.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-export CUDA_VISIBLE_DEVICES="3,4,0"
+# export CUDA_VISIBLE_DEVICES="0"
+export CUDA_VISIBLE_DEVICES="0,3,4"
 # export MASTER_ADDR=130.212.4.133
 # export MASTER_PORT=29500
 # export NCCL_DEBUG=INFO
@@ -9,9 +9,9 @@ export CUDA_VISIBLE_DEVICES="3,4,0"
 # export NCCL_SOCKET_IFNAME=eno1
 
 ZERO_STAGE=1
-NUM_EPOCHS=2
+NUM_EPOCHS=3
 NUM_CAPTIONS=10
-SUBSET_SIZE=0.001
+SUBSET_SIZE=0.002
 
 ENCODER_NUM_HIDDEN_LAYERS=12 # 
 DECODER_NUM_HIDDEN_LAYERS=12 # 12x768, 25x1600, 40x1600, 80x1600, 160x1600
@@ -19,17 +19,17 @@ HIDDEN_SIZE_ENCODER=768
 HIDDEN_SIZE_DECODER=768
 GRADIENT_ACCUMULATION_STEPS=1
 
-EXPERIMENT_NAME="VATEX-TESTING_v4"
+EXPERIMENT_NAME="VATEX-TESTING-PER-EPOCH-METRICS_v10"
 DATA_DIR="/data2/juve/dataset/vatex/npz_datasets/VATEX_8_frames"
 OUTPUT_DIR="/data2/juve/yo/"
 
 NUM_NODES=1   # Number of nodes
 NUM_GPU=3     # Number of GPUs per node
-MICRO_BATCH=1 # PER GPU
+# MICRO_BATCH=1 # PER GPU
 
 WORLD_SIZE=$((NUM_NODES * NUM_GPU))
-BATCH_SIZE=$((WORLD_SIZE * MICRO_BATCH)) # Total batch size acrross all GPUs and nodes
-
+# BATCH_SIZE=$((WORLD_SIZE * MICRO_BATCH)) # Total batch size acrross all GPUs and nodes
+BATCH_SIZE=2
 # PRETRAINED_MODEL="/data1/juve/training_artifacts/vatex_100/polynomial/vatex_1.0prcnt_s24_10caps_lr1e-05_30_epochs_power_1.4_end_1e_8/model_saved_files/epoch_3"
 PRETRAINED_ENC="facebook/timesformer-base-finetuned-k600"
 PRETRAINED_DEC="openai-community/gpt2"
@@ -38,7 +38,7 @@ TOKENIZER="gpt2"
 
 # torchrun --nproc_per_node=3 --nnodes=1 --node_rank=0 main_deepspeed.py  --num_gpus $NUM_GPU \
     # --pretrained_model $PRETRAINED_MODEL \
-deepspeed main_deepspeed_gpt5_fix.py --num_gpus $NUM_GPU \
+deepspeed main_deepspeed_gpt5_fix_copy.py --num_gpus $NUM_GPU \
     --world_size $WORLD_SIZE -ep $NUM_EPOCHS -ss $SUBSET_SIZE --num_captions $NUM_CAPTIONS \
     --experiment_name $EXPERIMENT_NAME --batch_size $BATCH_SIZE \
     --data_dir $DATA_DIR --output_dir $OUTPUT_DIR \
@@ -55,6 +55,10 @@ deepspeed main_deepspeed_gpt5_fix.py --num_gpus $NUM_GPU \
     --do_test --num_qualitative 100 \
     --greedy_decoding \
     --do_train --do_val \
+    # --resume_from_checkpoint 12 \
+    # epoch 9 NEXT
+    # --do_train --do_val \
+    # --resume_from_universal /data2/juve/yo/psc_ref_run/universal_epoch_5 \
     # --resume_from_checkpoint 0 \
     # --decode_strategy sample --top_k 50 --top_p 0.9 --temperature 1.0 --no_repeat_ngram_size 3 \
     # --decode_strategy beam --num_beams 5 --length_penalty 1.0 --no_repeat_ngram_size 3 --temperature 1.0 \
